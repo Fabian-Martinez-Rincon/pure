04b3873e19cc6abb450fe8fe196b12095eb26d53
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2025-04-17 14:39:19 -0300

    Actualizando los links

--- packages/pure/schemas/share.ts
+++ packages/pure/schemas/share.ts
@@ -1,8 +1,9 @@
 import { z } from 'astro/zod'
 
-export const shareList = [''] as const
+export const shareList = ['youtube', 'github'] as const
 
 export const ShareSchema = () =>
   z
     .array(z.enum(shareList))
+    .default(['youtube', 'github'])
     .describe('Options for sharing content on social media platforms.')
